12c66afd4ce3432d102a1e1042532eb8318e6eca
bjornburrell <burrelbn@mail.uc.edu>
2021-09-12 22:28:30 -0400

    updated powershell files

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,10 +1,29 @@
-﻿function getIP {
+﻿#Powershell script for Project 3 in IT3038c
+#Bjorn Burrell
+
+function getIP 
+{
 
 (Get-NetIPAddress).IPv4Address | Select-String "192"
 
 }
 
-$IP = getIP
+function getDate
+ 
+{
 
-Write-Host("This machine's IP is $IP")
-Write-Host("This machine's IP is {0}" -f $IP)
+(Get-Date)
+
+}
+
+$IP = getIP
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$PSV = $Host.Version.Major
+$DATE = getDate
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is $HOSTNAME. Powershell version is $PSV. The date is $DATE."
+
+#Write-Host("This machine's IP is $IP")
+#Write-Host("Test... $Body")
+
+Send-MailMessage -To "burrelbn@mail.uc.edu" -From "bjorn746@gmail.com" -Subject "IT3038C Project 3" -Body $BODY smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
