2631384db3b098a90cf096c8f6815bef6d39cfd3
Bjorn Burrell <bjornburrell@MacBook-Air.localdomain>
2021-09-26 21:29:00 -0400

    Adding Lab 5 changes...

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,6 +1,3 @@
-#Lab 5
-#Bjorn Burrell
-#Assignment: Write a ‘guess the number game’ where a random number is generated and the user must guess the number. The program says if their number is too high or too low until the right answer is guessed.
 
 import random
 
@@ -9,7 +6,7 @@ number = random.randint(1,99)
 
 print('Mr.Both, the best teacher ever, is thinking of a number between 1 and 99.... guess until you are right!')
 
-while numberGuess is False:
+while numberGuessed is False:
     print('Enter a number...')
     guess = int(input())
     if guess < number:
